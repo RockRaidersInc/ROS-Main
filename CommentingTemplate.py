a290fdece88d65347076a88727306acf4ae1732f
ChrisVech <chris10is@optonline.net>
2017-01-28 15:33:52 -0500

    Adds documentation template for ROS nodes

---
+++ CommentingTemplate.py
@@ -0,0 +1,65 @@
+##############################################################################
+
+#File Name (CommentingTemplate.py)
+#Authors (put your name here if you make any major revisions)
+
+'''
+Description (what does this node do?)
+
+'''
+'''
+How do you call this node?
+rosrun <PackageName> <NodeName (usually the name of the file)> <parameters>
+
+'''
+
+#Topics this node is subscribed to
+#Topics this node publishes to
+#Services this node uses
+#Other dependencies?
+
+##############################################################################
+
+#include
+...
+...
+
+#CONSTANTS (organize these as necessary)
+#names for constants should be in ALL CAPS
+...
+...
+
+##############################################################################
+
+#Setup
+#every node should have one
+def Setup(params):
+    '''
+    body of setup function
+    Run all prequisite code to prepare for the main loop.
+    MAKE SURE YOUR EDITOR USES 4 SPACES FOR TABS
+    '''
+
+#Loop
+#every node should have one
+def Loop(params):
+    '''
+    body of the main loop
+    MAKE SURE YOUR EDITOR USES 4 SPACES FOR TABS
+    '''
+
+##############################################################################
+
+#Helper Functions
+
+'''
+function header
+what does it return? what parameters? general description.
+'''
+def Foo(params):
+    '''
+    body of function
+    MAKE SURE YOUR EDITOR USES 4 SPACES FOR TABS
+    '''
+
+##############################################################################
