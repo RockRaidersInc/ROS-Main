7d82b429d919f8117d9ce0f7b3f5f671d538b2a2
NickSquared <curtin@rpi.edu>
2017-03-04 22:01:34 -0500

    Added edge detection to tennis_detection.py

--- vision/src/tennis_detection.py
+++ vision/src/tennis_detection.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('TennisBalls.mp4')
 yellow = np.uint8([[[255,255,0 ]]])
 hsv_yellow = cv2.cvtColor(yellow,cv2.COLOR_BGR2HSV)
 print hsv_yellow
@@ -10,22 +10,23 @@ while(1):
 
     # Take each frame
     _, frame = cap.read()
-    
-    '''
-    cimg = cv2.cvtColor(frame,cv2.COLOR_GRAY2BGR)
-    circles = cv2.HoughCircles(hsv,cv2.HOUGH_GRADIENT,1,40,param1=200,param2=30,minRadius=0,maxRadius=0)
+    if frame == None:
+        break
 
-    circles = np.uint16(np.around(circles))
-
-    for i in circles[0,:]:
-    	cv2.circle(hsv,(i[0],i[1],i[2],(0,255,0,2)))
-    	cv2.circle(hsv,(i[0],i[1]),2,(0,0,255),3)
-	'''
+    gray = cv2.medianBlur(frame,5)
+    gray = cv2.cvtColor(gray,cv2.COLOR_BGR2GRAY)
+    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,40,param1=200,param2=40,minRadius=0,maxRadius=0)
+    if circles!=None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+        	cv2.circle(gray,(i[0],i[1]),i[2],(0,255,0),2)
+        	cv2.circle(gray,(i[0],i[1]),2,(0,0,255),3)
+	
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # define range of blue color in HSV
-    lower_yellow = np.array([0,100,00])
+    # define range of yellow color in HSV
+    lower_yellow = np.array([0,100,0])
     upper_yellow = np.array([50,255,255])
 
     # Threshold the HSV image to get only yellow colors
@@ -37,6 +38,7 @@ while(1):
     cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
     cv2.imshow('res',res)
+    cv2.imshow('detected circles',gray)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
