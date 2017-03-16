061fa47dc6ac78a9b2015e9aee7d7bd951abc8d3
NickSquared <curtin@rpi.edu>
2017-03-15 23:04:51 -0400

    Adjusted the values for tennis ball color detection and changed circle detection to only run on the tennis ball mask so that false positives are less likely.

--- vision/src/tennis_detection.py
+++ vision/src/tennis_detection.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from time import sleep as wait
 
 cap = cv2.VideoCapture('TennisBalls.mp4')
 yellow = np.uint8([[[255,255,0 ]]])
@@ -13,14 +14,6 @@ while(1):
     if frame == None:
         break
 
-    gray = cv2.medianBlur(frame,5)
-    gray = cv2.cvtColor(gray,cv2.COLOR_BGR2GRAY)
-    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,40,param1=200,param2=40,minRadius=0,maxRadius=0)
-    if circles!=None:
-        circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-        	cv2.circle(gray,(i[0],i[1]),i[2],(0,255,0),2)
-        	cv2.circle(gray,(i[0],i[1]),2,(0,0,255),3)
 	
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -35,13 +28,30 @@ while(1):
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    cv2.imshow('detected circles',gray)
+    '''
+    gray = cv2.medianBlur(frame,5)
+    gray = cv2.cvtColor(gray,cv2.COLOR_BGR2GRAY)
+    '''
+    circles = cv2.HoughCircles(mask,cv2.HOUGH_GRADIENT,1,25,param1=100,param2=12,minRadius=0,maxRadius=50)
+    if circles!=None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            
+            cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+            cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
+            
+
+    cv2.imshow('Detected tennis balls',frame)
+    cv2.imshow('Mask',mask)
+    cv2.imshow('Res',res)
+    #cv2.imshow('detected circles',gray)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
+
+    #Wait for a moment to destroy frame
+    wait(0.05)
+
 
 
 cv2.destroyAllWindows()
