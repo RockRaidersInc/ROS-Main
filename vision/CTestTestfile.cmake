f8463a6df8c6c74246091c431ab70368e072521f
NickSquared <curtin@rpi.edu>
2017-03-25 14:49:24 -0400

    Fixed vision to run in ROS, and updated package.xml/catkin files

---
+++ vision/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nick/catkin_ws/src/ROS-Main/vision
+# Build directory: /home/nick/catkin_ws/src/ROS-Main/vision
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- vision/src/tennis_detection.py
+++ vision/src/tennis_detection.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 ##############################################################################
 
 #File name: tennis_detection.py
@@ -19,6 +21,7 @@ rosrun <vision> <tennis_detection> <parameters>
 ##############################################################################
 
 #include
+import rospy
 import cv2
 import numpy as np
 from time import sleep as wait
@@ -33,56 +36,59 @@ from time import sleep as wait
 def Setup():
 
     cap = cv2.VideoCapture('TennisBalls.mp4')
-    yellow = np.uint8([[[255,255,0 ]]])
+    yellow = np.uint8([[[255,255,0]]])
     hsv_yellow = cv2.cvtColor(yellow,cv2.COLOR_BGR2HSV)
     print hsv_yellow
-
+    return cap
 
 #Loop
 #every node should have one
 def Loop(cap):
+    while(cap.read()):
+        # Take each frame
+        _, frame = cap.read()
+        if frame == None:
+            return
+        # Convert BGR to HSV
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Take each frame
-    _, frame = cap.read()
-    if frame == None:
-        return
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # define range of yellow color in HSV
+        lower_yellow = np.array([0,100,0])
+        upper_yellow = np.array([50,255,255])
 
-    # define range of yellow color in HSV
-    lower_yellow = np.array([0,100,0])
-    upper_yellow = np.array([50,255,255])
+        # Threshold the HSV image to get only yellow colors
+        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
-    # Threshold the HSV image to get only yellow colors
-    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        # Bitwise-AND mask and original image
+        res = cv2.bitwise_and(frame,frame, mask= mask)
 
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+        '''
+        gray = cv2.medianBlur(frame,5)
+        gray = cv2.cvtColor(gray,cv2.COLOR_BGR2GRAhY)
+        '''
 
-    '''
-    gray = cv2.medianBlur(frame,5)
-    gray = cv2.cvtColor(gray,cv2.COLOR_BGR2GRAhY)
-    '''
+        mask = cv2.medianBlur(mask,5)
 
-    circles = cv2.HoughCircles(mask,cv2.HOUGH_GRADIENT,1,25,param1=100,param2=12,minRadius=0,maxRadius=50)
-    if circles!=None:
-        circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            
-            cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
-            cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
-            
-    cv2.imshow('Detected tennis balls',frame)
-    cv2.imshow('Mask',mask)
-    cv2.imshow('Res',res)
-    #cv2.imshow('detected circles',gray)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        cv2.destroyAllWindows()
-        return
 
-    #Wait for a moment to destroy frame
-    wait(0.05)
+        circles = cv2.HoughCircles(mask,cv2.HOUGH_GRADIENT,1,25,param1=100,param2=12,minRadius=0,maxRadius=50)
+        if circles!=None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0,:]:
+                
+                cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+                cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
+                
+        cv2.imshow('Detected tennis balls',frame)
+        cv2.imshow('Mask',mask)
+        cv2.imshow('Res',res)
+        #cv2.imshow('detected circles',gray)
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            cv2.destroyAllWindows()
+            return
+
+        #Wait for a moment to destroy frame
+        wait(0.05)
 
 ##############################################################################
 
@@ -96,9 +102,13 @@ what does it return? what parameters? general description.
 #TODO: Add some way to calibrate for the color white to be able to compensate for other lighting conditions
 
 def Foo():
+    pass
     '''
     body of function
     MAKE SURE YOUR EDITOR USES 4 SPACES FOR TABS
     '''
 
 ##############################################################################
+
+if __name__ == '__main__':
+    Loop(Setup())
