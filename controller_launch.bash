085c8df6cd30ac204b656c0c7cba28f12f31e23d
Patrick Love <patricklove@comcast.net>
2017-04-04 19:10:39 -0400

    More script beautifying and automated testing

--- controller_launch.bash
+++ controller_launch.bash
@@ -20,7 +20,11 @@ if [ "$ROVER_IP" = "192.168.1.0" -a "$MY_IP" != "192.168.1.9" ]
     then
     # Warn user if they perhaps forgot to run the antenna setup on this end
     echo "WARNING: Defualt rover IP used, but computer's IP is not default controller IP"
+    read -p "Do you want to continue launch? (Y/N) " yesno 
+    if [ "$yesno" != "y" -a "$yesno" != "Y" ]; then
+        exit 1;
+    fi
 fi
-export ROS_HOSTNAME=$MY_IP
+export ROS_IP=$MY_IP
 # Launch the actual code
 roslaunch controller.launch
--- robot_launch.bash
+++ robot_launch.bash
@@ -1,5 +1,15 @@
 #!/bin/bash
 source /opt/ros/indigo/setup.bash
 source ../devel/setup.bash
-export ROS_HOSTNAME=$(hostname -I | grep -Eo '([0-9]*\.){3}[0-9]*')
+MY_IP=$(hostname -I | grep -Eo '([0-9]*\.){3}[0-9]*')
+echo $MY_IP
+if [ "$MY_IP" = "192.168.1.0" ]; then
+    echo "Detected configuration for antenna use, if this is incorrect please check your wireless connection"
+else
+    echo "Detected configuration for local network use, if this is incorrect please verify that you have properly configured your IP and disabled wireless"
+fi
+
+read -p "Press [ENTER] to continue launch..."
+
+export ROS_IP=$MY_IP
 roslaunch rover.launch
