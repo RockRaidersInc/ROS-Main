ea3f8edf85ba5de2dae14ddf015aa598c654199a
Patrick Love <patricklove@comcast.net>
2017-04-04 18:16:17 -0400

    Made script nicer

---
+++ antenna_config.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+    echo "Please specify configuration type (rover or base)"
+    exit
+fi
+
+# Determine desired static IP
+if [ "$1" = "rover" ]; then
+    STATIC_IP=192.168.1.0
+elif [ "$1" = "base" ]; then
+    STATIC_IP=192.168.1.9
+else
+    echo "Valid configuations are one of 'rover' or 'base'"
+    exit
+fi
+
+# Check network device
+ETH_DEVICE=eth0
+if [ $# -ge 2 ]; then
+    ETH_DEVICE=$2
+fi
+ifconfig $ETH_DEVICE &>/dev/null
+if [ $? -ne 0 ]; then #Device was not found
+    echo "ERROR: Network device "$ETH_DEVICE" could not be found!"
+    exit 1
+fi
+
+# Configure IP
+sudo ifconfig $ETH_DEVICE $STATIC_IP netmask 255.255.255.0
+
+# Print confirmation
+echo $ETH_DEVICE" configured for "$1" antenna use."
--- controller_launch.bash
+++ controller_launch.bash
@@ -1,11 +1,26 @@
 #!/bin/bash
-if [ $# -eq 1 ]
+# Defualt rover IP when used with antenna
+ROVER_IP=192.168.1.0
+
+if [ $# -eq 1 ] # User overrode default rover IP
     then
-    source /opt/ros/indigo/setup.bash
-    source ../devel/setup.bash
-    echo export ROS_MASTER_URI=http://$1:11311
-    echo export ROS_HOSTNAME=$(hostname -I | grep -Eo '([0-9]*\.){3}[0-9]*')
-    roslaunch controller.launch
+    ROVER_IP=$1
+    echo "Rover IP set to "$ROVER_IP
 else
-    echo No robot IP address specified
+    echo "No rover IP set, using default (192.168.1.0)"
 fi
+
+source /opt/ros/indigo/setup.bash
+source ../devel/setup.bash
+
+# Configure ROS environment
+export ROS_MASTER_URI=http://"$ROVER_IP":11311
+MY_IP=$(hostname -I | grep -Eo '([0-9]*\.){3}[0-9]*')
+if [ "$ROVER_IP" = "192.168.1.0" -a "$MY_IP" != "192.168.1.9" ]
+    then
+    # Warn user if they perhaps forgot to run the antenna setup on this end
+    echo "WARNING: Defualt rover IP used, but computer's IP is not default controller IP"
+fi
+export ROS_HOSTNAME=$MY_IP
+# Launch the actual code
+roslaunch controller.launch
