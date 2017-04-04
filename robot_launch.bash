687d38ec28d8968d003296b6cdb3d33fe44c1053
Patrick Love <patricklove@comcast.net>
2017-04-04 19:45:43 -0400

    Moved IP accquisition into script
    Simplified launch process by removing 128.113.224.27 2620:0:2820:a08:7841:849a:d66b:bc1f 2620:0:2820:a08:527b:9dff:fef5:921f  step and simply printing the IP in the rover launch script when appropriate

--- robot_launch.bash
+++ robot_launch.bash
@@ -13,13 +13,13 @@ source ../devel/setup.bash
 
 
 MY_IP=$(hostname -I | grep -Eo '([0-9]*\.){3}[0-9]*')
-echo $MY_IP
 if [ "$MY_IP" = "192.168.1.0" ]; then
     echo "Detected configuration for antenna use, if this is incorrect please check your wireless connection"
 else
     echo "Detected configuration for local network use, if this is incorrect please verify that you have properly configured your IP and disabled wireless"
+    echo "If this is correct, the rover IP is $MY_IP"
+    echo "You will need this for the base station setup"
 fi
-
 read -p "Press [ENTER] to continue launch..."
 
 export ROS_IP=$MY_IP
