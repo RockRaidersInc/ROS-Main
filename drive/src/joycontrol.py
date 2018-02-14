e920f5e1af3d8e14ed93c9dbf09825fd2a15bc3a
siddharth <siddharth.suri0@gmail.com>
2018-02-14 16:16:13 -0500

    Cleaned up drive code. Added encoders to motornode. Each motor publishes individually

--- drive/src/joycontrol.py
+++ drive/src/joycontrol.py
@@ -1,62 +1,33 @@
-#!/usr/bin/python
-
-#File Name main.py
-#Authors Matthew Raneri
-
-'''
-Publishes input from the logitech controller (should be device js2)
-'''
-'''
-How do you call this node?
-rosrun logitech_controller main.py
-'''
-
-# Topics this node is publishing to:
-# logitech controller
-# publishes a string with a space, first parameter is button name or axis name
-# second parameter is the value it outputs
+#!/usr/bin/env python
 
 import rospy
-import joystick_read
-
+import roboclaw
 from geometry_msgs.msg import Vector3
+from std_msgs.msg import String
+from sensor_msgs.msg import Joy
 
-joyname = 'js0'
+class joycontrol:
+	left_pub = None
+	right_pub = None
 
-# publish controller info
+	left_y = 0
+	right_y = 0
 
-class joynode:
-	pub = None
-	jsdev = None
-	x=0
-	y=0
-	z=0
-	k=0
+	LEFT_STICK_Y_INDEX = 1;
+	RIGHT_STICK_Y_INDEX = 3;
 
-	def __init__(self):
-		self.pub = rospy.Publisher("beat", Vector3, queue_size=10)
-    		rospy.init_node("logitech_controller", anonymous=True)
-   		self.jsdev = joystick_read.connect(joyname)
-    		self.x=0
-    		self.y=0
-    		self.z=0
-    		self.k=25
+
+	def __init__(self, n):
+		self.left_pub = rospy.Publisher('left', Vector3, queue_size = 1);
+		self.right_pub = rospy.Publisher('right', Vector3, queue_size = 1);
+
+		rospy.Subscriber("joy", Joy, self.callback)
+
+		while not rospy.is_shutdown():
+			self.left_pub.publish(Vector3(left_y, left_y, 0));
+			self.right_pub.publish(Vector3(right_y, right_y, 0));
+
+	def callback(self, data):
+		left_y = (1 + data.axes[LEFT_STICK_Y_INDEX]) * 64;
+		right_y = (1 + data.axes[RIGHT_STICK_Y_INDEX]) * 64;
 		
-	def loop(self):
-		rate = rospy.Rate(200) # 200hz cycle
-    		while not rospy.is_shutdown():
-        		# process controller input
-        		inputinfo = joystick_read.wait_for_changes(self.jsdev)
-			if inputinfo[0]=='y':
-				self.x=float(inputinfo[1])*-self.k
-			elif inputinfo[0]=='x':
-				self.y = float(inputinfo[1])*self.k
-			elif inputinfo[0]=='rx':
-				self.z = float(inputinfo[1])*-self.k
-			self.pub.publish(Vector3(self.x,self.y,self.z))
-			print("published x %i, y %i, z %i"%(self.x,self.y,self.z))
-        		rate.sleep()
-
-if __name__ == "__main__":
-	p = joynode()
-	p.loop()
--- drive/src/motornode.py
+++ drive/src/motornode.py
@@ -4,6 +4,7 @@ import rospy
 import roboclaw
 from geometry_msgs.msg import Vector3
 from std_msgs.msg import String
+from std_msgs.msg import Int32
 
 
 class motornode:
@@ -14,79 +15,102 @@ class motornode:
 	y=64
 	timeout = 1000
 	pub = None
+	enc_pub = None
 
-	def __init__(self,n):
+	def __init__(self,n,m1_name=n+'M1',m2_name=n+'M2',useEnc=False):
 		self.name = n
 		rospy.init_node('motornode', anonymous=True)
 
-    		self.pub = rospy.Publisher('usb', String, queue_size = 1)
+		self.pub = rospy.Publisher('usb', String, queue_size = 1)
 
-    		rospy.Subscriber(self.name, Vector3, self.callback)
-    		rospy.Subscriber(self.name+'device', String, self.setdevice)
+		rospy.Subscriber(self.name, Vector3, self.callback)
+		rospy.Subscriber(self.name+'device', String, self.setdevice)
+		rospy.Subscriber(m1_name, Int32, self.callbackM1)
+		rospy.Subscriber(m2_name, Int32, self.callbackM2)
+		
+		if (useEnc == True):
+			self.enc_pub = rospy.Publisher(self.name+'encoder', Vector3, queue_size=3)
     
-    		print("start finished")
+		print("start finished")
 		while not rospy.is_shutdown():
 			self.connect()
-			self.normal()
+			self.normal(useEnc)
 		self.connect()
 
-    	def callback(self,data):
-    		self.timeout = 1000
+	def callback(self,data):
+		self.timeout = 1000
 
-    		self.x = int(data.x)
-    		self.y = int(data.y)
-    		print("(x,y) = (%i,%i)"%(self.x,self.y))
+		self.x = int(data.x)
+		self.y = int(data.y)
+		print("(x,y) = (%i,%i)"%(self.x,self.y))
+
+	def callbackM1(self, data):
+		self.timeout = 1000
+		self.x = data
+
+	def callbackM2(self, data):
+		self.timeout = 1000
+		self.y = data
 
 
 
 	def setdevice(self,data):
-    		self.device = str(data.data)
-    		rospy.loginfo("device set to: "+self.device)
-    		
+			self.device = str(data.data)
+			rospy.loginfo("device set to: "+self.device)
+			
 
 	def connect(self):
 		print("trying to connect")
 		connect = False
-        	wait = rospy.Rate(1)
+		wait = rospy.Rate(1)
 		while (connect==False):
-    			try:
-				print self.device
-    				roboclaw.Open(self.device,115200)
-       		 		connect = True
-				self.pub.publish("connect "+self.device)
-				print ("connected")
-				break
-    			except:
-				print("no device "+ self.device)
-				self.pub.publish("disconnect "+ self.device)
-			wait.sleep()
+				try:
+					print(self.device)
+					roboclaw.Open(self.device,115200)
+					connect = True
+					self.pub.publish("connect "+self.device)
+					roboclaw.ResetEncoders(self.address);
+					print ("connected")
+					break
+				except:
+					print("no device "+ self.device)
+					self.pub.publish("disconnect "+ self.device)
+					wait.sleep()
 
 	def setmotor (self,m,n):
-   		
-      		roboclaw.ForwardBackwardM1(self.address,m)
-      		roboclaw.ForwardBackwardM2(self.address,n)
+		
+			roboclaw.ForwardBackwardM1(self.address,m)
+			roboclaw.ForwardBackwardM2(self.address,n)
 
-   	 
+	 
 		
 	
-	def normal(self):
-  		while not rospy.is_shutdown():
+	def normal(self, useEnc):
+		while not rospy.is_shutdown():
 			if (self.timeout > 0):
 				print("setting motor to values %i, %i"%(self.x,self.y))
+				self.enc(useEnc)
 				try:
-      					self.setmotor(self.x,self.y)
+						self.setmotor(self.x,self.y)
 				except:
 					er = 1
 					break
-       				self.timeout -=1
-        		else:
-				try:
-	    				self.setmotor(64,64)
-				except:
-					er = 1
-					break
+					self.timeout -=1
+				else:
+					try:
+						self.setmotor(64,64)
+					except:
+						er = 1
+						break
     
 	
+	def enc(self, useEnc):
+		if (useEnc == False):
+			return
+		enc1 = roboclaw.ReadEncM1(self.address)
+		enc2 = roboclaw.ReadEncM2(self.address)
+
+		self.enc_pub.publish(Vector3(enc1[1], enc2[1], 0))
 
 if __name__ == '__main__':
-	controller = motornode('steer')
+	controller = motornode('steer', True)
