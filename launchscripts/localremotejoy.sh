0d18fa8415fa9928658dfd18b68b4eea96f4bde2
Siddharth <siddharth.suri0@gmail.com>
2019-01-26 16:47:01 -0500

    Cleanup. Yell at sid if something should go back

--- launchscripts/localremotejoy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /opt/ros/kinetic/setup.bash
-#set to your workspace setup file
-source ~/ws/devel/setup.bash
-
-
-roslaunch drive joystick.launch
-
-read
--- ui/scripts/test.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-source /opt/ros/kinetic/setup.bash
-source /opt/ros/kinetic/setup.bash
-
-python firstdraft.py
-
