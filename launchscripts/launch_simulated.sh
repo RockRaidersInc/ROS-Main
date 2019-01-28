026d27554c7b8d63b8351db7466aaeda95d704fa
David <daweim0@gmail.com>
2019-01-28 17:38:54 -0500

    restored part of the launchscripts folder

---
+++ launchscripts/launch_simulated.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+echo $SCRIPTPATH
+cd $SCRIPTPATH
+
+#source /opt/ros/kinetic/setup.bash
+source ../../devel/setup.bash
+
+# add some gazebo enviornment variables
+cd ../model_database
+source fix_gazebo_paths.sh
+cd ../launchscripts
+
+# why do we need the ip address?
+text1=$(curl -s http://whatismyip.akamai.com/)
+
+echo "IP address is:"
+echo $text1
+echo 
+echo
+roslaunch launch_files start_simulator.launch
+
---
+++ launchscripts/start_keyboard_joystick.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+echo $SCRIPTPATH
+cd $SCRIPTPATH
+
+#source /opt/ros/kinetic/setup.bash
+source ../../devel/setup.bash
+
+rosrun teleop_twist_keyboard teleop_twist_keyboard.py cmd_vel:=/twist
