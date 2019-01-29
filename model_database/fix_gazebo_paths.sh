7b938df53cef96ccc7c4085968b32010a746e79a
David <daweim0@gmail.com>
2019-01-29 13:18:05 -0500

    imu cleanup, sim terrain stuff

--- model_database/fix_gazebo_paths.sh
+++ model_database/fix_gazebo_paths.sh
@@ -1,3 +1,4 @@
-
+source /usr/share/gazebo/setup.sh
+export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:$(pwd)
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(pwd)
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$(pwd)/../../devel/lib/
