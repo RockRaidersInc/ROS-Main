e5f34ef0f78f1ba1bf29de1a115c597fa9524a29
wus7 <wus7@ubuntu.ubuntu-domain>
2019-02-18 21:41:41 +0000

    Added gps waypoint navigation. Still needs debugging because the odometry efk as incredibly unstable and move_base is not able to navigate to the goals generated by geonav_transform because the odometry localization is too unstable.

--- autonomous_nav/src/send_goal.py
+++ autonomous_nav/src/send_goal.py
@@ -3,12 +3,14 @@
 
 import rospy
 import actionlib
+import sys
 from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction
+import geonav_transform.geonav_conversions as gc
 
 
 def make_goal(x=0, y=0, thetaZ=1., w=0.):
 	goal = MoveBaseGoal()
-	goal.target_pose.header.frame_id = 'odom'
+	goal.target_pose.header.frame_id = 'map'
 	goal.target_pose.header.stamp = rospy.Time.now()
 	goal.target_pose.pose.position.x = x
 	goal.target_pose.pose.position.y = y
@@ -16,14 +18,37 @@ def make_goal(x=0, y=0, thetaZ=1., w=0.):
 	goal.target_pose.pose.orientation.w = w
 	return goal
 
+def main2():
+	rospy.init_node('send_goal')
+	client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+	rospy.loginfo('Waiting for server')
+	client.wait_for_server()
+
+	olat=42.729957
+	olon=-73.679659
+	target_lat = 42.73
+	target_lon = -73.679659
+	xg2,yg2 = gc.ll2xy(target_lat,target_lon,olat,olon)
+	checkpoint = (xg2,yg2)
+	rospy.loginfo('Making goal with (lat,lon): ({},{})'.format(target_lat,target_lon))
+	rospy.loginfo('(lat,lon): ({},{}) converts to (x,y): ({},{})'.format(target_lat,target_lon,xg2,yg2))
+
+	goal = make_goal(*checkpoint)
+	rospy.loginfo('Sending goal')
+	client.send_goal(goal)
+	rospy.loginfo('Waiting for results')
+	client.wait_for_result()
+	sys.exit()
+
+
 def main():
 	rospy.init_node('send_goal')
 	client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
 	rospy.loginfo('Waiting for server')
 	client.wait_for_server()
-	checkpoint = (10,0)
+	checkpoint = (0,0)
 	rospy.loginfo('Making goal with checkpoint: {}'.format(checkpoint))
-	goal = make_goal(10, 0)
+	goal = make_goal(*checkpoint)
 	rospy.loginfo('Sending goal')
 	client.send_goal(goal)
 	rospy.loginfo('Waiting for results')
@@ -32,4 +57,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+	main2()
