db0eb517c386a10682a4c22005206fb0ffbc3f86
wus7 <wus7@ubuntu.ubuntu-domain>
2019-02-23 15:22:43 +0000

    Added some scripts to make it easier to load GPS coordinates

--- autonomous_nav/src/utils/waypoint_nav.py
+++ autonomous_nav/src/utils/waypoint_nav.py
@@ -3,10 +3,19 @@
 import rospy
 import rospkg
 import actionlib
-import yaml
 from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction
 import geonav_transform.geonav_conversions as gc
 
+
+def set_mapviz_origin(orig_lat,orig_lon):
+	origin_name = rospy.get_param('local_xy_origin')
+	origins = rospy.get_param('local_xy_origins')
+	for origin in origins:
+		if origin_name == origin['name']:
+			origin['latitude'] = orig_lat
+			origin['longitude'] = orig_lon
+			break
+	rospy.set_param('local_xy_origins', origins)
 
 def make_goal(x=0, y=0, thetaZ=1, w=0.):
 	goal = MoveBaseGoal()
@@ -17,15 +26,6 @@ def make_goal(x=0, y=0, thetaZ=1, w=0.):
 	goal.target_pose.pose.orientation.z = thetaZ
 	goal.target_pose.pose.orientation.w = w
 	return goal
-
-def load_coordinates(filename):
-	rospack = rospkg.RosPack()
-	AT_file_path = rospack.get_path('autonomous_nav')
-	file_path = '{}/coordinates/{}.yaml'.format(AT_file_path, filename)
-	with open(file_path) as file:
-		coordinates = yaml.safe_load(file)
-	return coordinates
-
 
 class WaypointNavigator:
 	def __init__(self, orig_lat, orig_lon):
