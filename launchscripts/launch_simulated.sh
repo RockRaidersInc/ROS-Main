21e57197dd505a0ba099ec5d1b4a01342095a444
David <daweim0@gmail.com>
2019-03-20 21:42:03 -0400

    changed default map back to 86 field

--- launchscripts/launch_simulated.sh
+++ launchscripts/launch_simulated.sh
@@ -13,6 +13,11 @@ cd ../model_database
 source fix_gazebo_paths.sh
 cd ../launchscripts
 
+# gazebo caches terrain files here. It does a really bad job of
+# keeping different terrain files separate (sometimes the wrong
+# maps show up), so delete the cache before every run.
+rm -rf ~/.gazebo/paging/
+
 # why do we need the ip address?
 text1=$(curl -s http://whatismyip.akamai.com/)
 
