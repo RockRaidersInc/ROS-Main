bd077573b46217de8762ca5a410e447a2cb3e7eb
David <daweim0@gmail.com>
2019-04-27 14:35:31 -0400

    pre-merge add

--- arduino_code/Razor_AHRS/Razor_AHRS.ino
+++ arduino_code/Razor_AHRS/Razor_AHRS.ino
@@ -72,6 +72,7 @@
 
 /*
   "9DoF Razor IMU M0" hardware versions: SEN-14001
+
   Arduino IDE : Follow the same instructions as for the default firmware on 
   https://learn.sparkfun.com/tutorials/9dof-razor-imu-m0-hookup-guide 
   and use an updated version of SparkFun_MPU-9250-DMP_Arduino_Library from 
@@ -80,16 +81,20 @@
 
 /*
   "9DOF Razor IMU" hardware versions: SEN-10125 and SEN-10736
+
   ATMega328@3.3V, 8MHz
+
   ADXL345  : Accelerometer
   HMC5843  : Magnetometer on SEN-10125
   HMC5883L : Magnetometer on SEN-10736
   ITG-3200 : Gyro
+
   Arduino IDE : Select board "Arduino Pro or Pro Mini (3.3v, 8Mhz) w/ATmega328"
 */
 
 /*
   "9DOF Sensor Stick" hardware versions: SEN-10183, SEN-10321 and SEN-10724
+
   ADXL345  : Accelerometer
   HMC5843  : Magnetometer on SEN-10183 and SEN-10321
   HMC5883L : Magnetometer on SEN-10724
@@ -116,7 +121,11 @@
     [a|m|g|c|t] _a_ccelerometer, _m_agnetometer, _g_yro, magnetometerellipsoid_c_enter, magnetometerellipsoid_t_ransform.
     [x|y|z] x,y or z.
     [m|M|X|Y|Z] _m_in or _M_ax (accel or magnetometer), X, Y, or Z of transform (magnetometerellipsoid_t_ransform).
+
+
   "#p" - PRINT current calibration values.
+
+
   "#o<params>" - Set OUTPUT mode and parameters. The available options are:
   
       // Streaming output
@@ -160,7 +169,11 @@
       "#oe1" - Enable ERROR message output.
       "#oec" - Output ERROR COUNT.
       "#oem" - Output MATH ERROR COUNT.
+
+
   "#I" - Toggle INERTIAL-only mode for yaw computation.
+
+
   "#f" - Request one output frame - useful when continuous output is disabled and updates are
          required in larger intervals only. Though #f only requests one reply, replies are still
          bound to the internal 20ms (50Hz) time raster. So worst case delay that #f can add is 19.99ms.
@@ -171,6 +184,8 @@
          "#SYNCH<xy>\r\n" in response (so it's possible to read using a readLine() function).
          x and y are two mandatory but arbitrary bytes that can be used to find out which request
          the answer belongs to.
+
+
   ("#C" and "#D" - Reserved for communication with optional Bluetooth module.)
   
   Newline characters are not required. So you could send "#ob#o1#s", which
@@ -257,34 +272,37 @@ boolean output_errors = false;  // true or false
 // For the M0, only the extended magnetometer calibration seems to be really necessary if DEBUG__USE_DMP_M0 is set to true...
 // Accelerometer
 // "accel x,y,z (min/max) = X_MIN/X_MAX  Y_MIN/Y_MAX  Z_MIN/Z_MAX"
-#define ACCEL_X_MIN ((float) -277)
-#define ACCEL_X_MAX ((float) 264)
-#define ACCEL_Y_MIN ((float) -256)
-#define ACCEL_Y_MAX ((float) 278)
-#define ACCEL_Z_MIN ((float) -299)
-#define ACCEL_Z_MAX ((float) 235)
+float ACCEL_X_MIN = -250;
+float ACCEL_X_MAX = 250;
+float ACCEL_Y_MIN = -250;
+float ACCEL_Y_MAX = 250;
+float ACCEL_Z_MIN = -250;
+float ACCEL_Z_MAX = 250;
 
-// "magn x,y,z (min/max) = -511.00/581.00  -516.00/568.00  -489.00/486.00"
-//#define MAGN_X_MIN ((float) -511)
-//#define MAGN_X_MAX ((float) 581)
-//#define MAGN_Y_MIN ((float) -516)
-//#define MAGN_Y_MAX ((float) 568)
-//#define MAGN_Z_MIN ((float) -489)
-//#define MAGN_Z_MAX ((float) 486)
+// Magnetometer (standard calibration mode)
+// "magn x,y,z (min/max) = X_MIN/X_MAX  Y_MIN/Y_MAX  Z_MIN/Z_MAX"
+float MAGN_X_MIN = -600;
+float MAGN_X_MAX = 600;
+float MAGN_Y_MIN = -600;
+float MAGN_Y_MAX = 600;
+float MAGN_Z_MIN = -600;
+float MAGN_Z_MAX = 600;
 
-// Extended magn
-#define CALIBRATION__MAGN_USE_EXTENDED true
-const float magn_ellipsoid_center[3] = {-2.66188e+06, 352328, 445297};
-const float magn_ellipsoid_transform[3][3] = {{0.447561, 0.0381775, 0.0834373}, {0.0381775, 0.436948, 0.137178}, {0.0834373, 0.137178, 0.950937}};
+// Magnetometer (extended calibration mode)
+// Set to true to use extended magnetometer calibration (compensates hard & soft iron errors)
+boolean CALIBRATION__MAGN_USE_EXTENDED = false;
+float magn_ellipsoid_center[3] = {0, 0, 0};
+float magn_ellipsoid_transform[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
-// Extended magn (with Sennheiser HD 485 headphones)
-//#define CALIBRATION__MAGN_USE_EXTENDED true
-//const float magn_ellipsoid_center[3] = {72.3360, 23.0954, 53.6261};
-//const float magn_ellipsoid_transform[3][3] = {{0.879685, 0.000540833, -0.0106054}, {0.000540833, 0.891086, -0.0130338}, {-0.0106054, -0.0130338, 0.997494}};
-
+// Gyroscope
+// "gyro x,y,z (current/average) = .../OFFSET_X  .../OFFSET_Y  .../OFFSET_Z
+float GYRO_AVERAGE_OFFSET_X = 0.0;
+float GYRO_AVERAGE_OFFSET_Y = 0.0;
+float GYRO_AVERAGE_OFFSET_Z = 0.0;
 
 /*
 // Calibration example:
+
 // "accel x,y,z (min/max) = -277.00/264.00  -256.00/278.00  -299.00/235.00"
 float ACCEL_X_MIN = -277;
 float ACCEL_X_MAX = 264;
@@ -292,6 +310,7 @@ float ACCEL_Y_MIN = -256;
 float ACCEL_Y_MAX = 278;
 float ACCEL_Z_MIN = -299;
 float ACCEL_Z_MAX = 235;
+
 // "magn x,y,z (min/max) = -511.00/581.00  -516.00/568.00  -489.00/486.00"
 float MAGN_X_MIN = -511;
 float MAGN_X_MAX = 581;
@@ -299,14 +318,17 @@ float MAGN_Y_MIN = -516;
 float MAGN_Y_MAX = 568;
 float MAGN_Z_MIN = -489;
 float MAGN_Z_MAX = 486;
+
 // Extended magn
 boolean CALIBRATION__MAGN_USE_EXTENDED = true;
 float magn_ellipsoid_center[3] = {91.5, -13.5, -48.1};
 float magn_ellipsoid_transform[3][3] = {{0.902, -0.00354, 0.000636}, {-0.00354, 0.9, -0.00599}, {0.000636, -0.00599, 1}};
+
 // Extended magn (with Sennheiser HD 485 headphones)
 //boolean CALIBRATION__MAGN_USE_EXTENDED = true;
 //float magn_ellipsoid_center[3] = {72.3360, 23.0954, 53.6261};
 //float magn_ellipsoid_transform[3][3] = {{0.879685, 0.000540833, -0.0106054}, {0.000540833, 0.891086, -0.0130338}, {-0.0106054, -0.0130338, 0.997494}};
+
 //"gyro x,y,z (current/average) = -40.00/-42.05  98.00/96.20  -18.00/-18.36"
 float GYRO_AVERAGE_OFFSET_X = -42.05;
 float GYRO_AVERAGE_OFFSET_Y = 96.20;
@@ -1017,3 +1039,4 @@ void loop()
 #endif // DEBUG__ADD_LOOP_DELAY
 #endif
 }
+
