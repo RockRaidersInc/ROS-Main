5150109b1376483f6256ae755e15b3f4493d9a56
wus7 <wus7@ubuntu.ubuntu-domain>
2019-05-26 17:13:00 +0000

    added lane detection v4 which works with 480p

---
+++ vision/lane_detection_4.py
@@ -0,0 +1,260 @@
+#! /usr/bin/python
+
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import pickle
+import matplotlib.image as mpimg
+import rospy
+from sensor_msgs.msg import Image
+import time
+import sys
+from debug_utils import *
+import os
+
+from sklearn import svm
+import csv
+
+import rospy
+from autonomous_traversal.msg import Lane
+from geometry_msgs.msg import Vector3
+
+
+bridge = CvBridge()
+
+class LaneDetector:
+    def __init__(self):
+        self.cv2_img = None
+        # train svm
+        with open("hsv_colors_labeled.csv") as hsv_file:
+            readCSV = csv.reader(hsv_file, delimiter=',')
+            h = []
+            s = []
+            v = []
+            line = []
+            next(readCSV)  # skip the header line
+            for row in readCSV:
+                h.append(float(row[0]))
+                s.append(float(row[1]))
+                v.append(float(row[2]))
+                line.append(1 if int(row[3]) == 1 else -1)
+            
+            train_features = np.array([h, s, v]).transpose()
+            self.hsv_mean = train_features.mean(axis=0)
+            train_features -= self.hsv_mean
+            self.hsv_stdev = np.stdev = np.std(train_features, axis=0)
+            train_features /= self.hsv_stdev
+
+
+            class_labels = np.array(line)
+            self.clf = svm.SVC(kernel='linear', C = 1.0)
+            self.clf.fit(train_features, class_labels)
+
+            # predict the training set as a sanity check
+            correct = 0
+            for i in range(train_features.shape[0]):
+                if self.clf.predict(train_features[i].reshape([1, 3])) == class_labels[i]:
+                    correct += 1
+            correct_percent = float(correct) / train_features.shape[0]
+
+            self.SVM_normal_vect = np.array(self.clf.coef_).squeeze()
+            self.SVM_intercept = self.clf.intercept_
+
+            # predict the training set as a sanity check
+            correct = 0
+            for i in range(train_features.shape[0]):
+                feature = train_features[i]
+                prediction = np.dot(feature, self.SVM_normal_vect) + self.SVM_intercept
+                if (1 if prediction > 0 else -1) == class_labels[i]:
+                    correct += 1
+            if float(correct) / train_features.shape[0] != correct_percent:
+                print("SVM did not get entire training set correct. This means the SVM is probably not working properly")
+                print("got " + str(correct) + " right out of " + str(train_features.shape[0]) + " training points")
+
+    def hsv_select(self, img):
+        # use an SVM to select colors that are probably lane
+        hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV).astype(np.float32)
+        data_prepped = (hsv_img - self.hsv_mean) / self.hsv_stdev
+
+        line_vals = np.dot(hsv_img, self.SVM_normal_vect) + self.SVM_intercept
+        color_binary = np.where(line_vals > 125, 255, 0)
+        
+        # line_scores = self.clf.predict(hsv_img.reshape([-1, 3])).reshape(hsv_img.shape[:2])
+        # color_binary = (line_scores + 1) * (255. / 2)
+        return color_binary
+
+    def warp(self, img):
+        img_size = (img.shape[1], img.shape[0])
+
+        img_x_half = img.shape[1] / 2
+        img_y_half = img.shape[0] / 2
+        y_offset = 480 / 2 - 80*480.0/720.0
+        square_size_y = 0.508 * 100
+        square_size_x = 0.762 * 100
+        scale_factor = 50
+
+        src = np.float32([[640 - 387*640.0/1280.0, 480 - 81*480.0/720.0],  # bottom left
+                         [640 - 515*640.0/1280.0, 480 - 370*480.0/720.0],  # top left
+                         [640 - 865*640.0/1280.0, 480 - 371*480.0/720.0],  # top right
+                         [640 - 1020*640.0/1280.0, 480 - 65*480.0/720.0]]) # bottom right
+
+        dst = np.float32([[2 * scale_factor + img_x_half, 2 * scale_factor + img_y_half + y_offset],   # bottom left
+                          [2 * scale_factor + img_x_half, -2 * scale_factor + img_y_half + y_offset],   # top left
+                          [-2 * scale_factor + img_x_half, -2 * scale_factor + img_y_half + y_offset],    # top right
+                          [-2 * scale_factor + img_x_half, 2 * scale_factor + img_y_half + y_offset]])  # bottom right
+
+        M = cv2.getPerspectiveTransform(src, dst)
+
+        # inverse
+        Minv = cv2.getPerspectiveTransform(dst, src)
+
+        # create a warped image
+        warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+
+        roi_unwarped = np.array([[[0, 0]], [[0, img_size[0]]], [[img_size[1], img_size[0]]], [[img_size[1], 0]]], dtype=np.float32)
+        roi_warped = cv2.perspectiveTransform(roi_unwarped, M)
+
+        # unpersp = cv2.warpPerspective(warped, Minv, img_size, flags=cv2.INTER_LINEAR)
+        unpersp = img
+
+        return warped, roi_warped, Minv
+
+    def warp_points(self, point_array, img_shape):
+        img_size = [480, 640]
+
+        img_x_half = img_shape[1] / 2
+        img_y_half = img_shape[0] / 2
+        y_offset = 480 / 2 - 80*480.0/720.0
+        scale_factor = 50
+
+        src = np.float32([[640 - 387*640.0/1280.0, 480 - 81*480.0/720.0],  # bottom left
+                         [640 - 515*640.0/1280.0, 480 - 370*480.0/720.0],  # top left
+                         [640 - 865*640.0/1280.0, 480 - 371*480.0/720.0],  # top right
+                         [640 - 1020*640.0/1280.0, 480 - 65*480.0/720.0]]) # bottom right
+        dst = np.float32([[0.9144, -0.6096],     # bottom left
+                        [2.1336, -0.6096],       # top left
+                        [2.1336, 0.6096],  # top right
+                        [0.9144, 0.6096]])  # bottom right
+
+        M = cv2.getPerspectiveTransform(src, dst)
+
+        roi_unwarped = np.array([[[img_size[1]*.1, img_size[0]*.1]], 
+                                [[ img_size[1]*.1, img_size[0]*.9]], 
+                                [[ img_size[1]*.9, img_size[0]*.9]], 
+                                [[ img_size[1]*.9, img_size[0]*.1]]], dtype=np.float32)
+        roi_warped = cv2.perspectiveTransform(roi_unwarped, M)
+
+        point_array = point_array.transpose()
+        reshaped_point_array = point_array.reshape([point_array.shape[0], 1, point_array.shape[1]]).astype(np.float32)
+        warped_points = cv2.perspectiveTransform(reshaped_point_array, M)
+
+        return warped_points, roi_warped
+
+    def publish_lane_pts(self, roi, points):
+        try:
+            roi = np.array(roi).squeeze()
+            points = np.array(points).squeeze()
+            # print points.shape
+            lane = Lane()
+
+            lower_x_bound = 1.9144
+            upper_x_bound = 5.
+            lower_y_bound = -2.
+            upper_y_bound = 2.
+
+            lane.bound_polygon.append(Vector3(lower_x_bound,lower_y_bound,0))
+            lane.bound_polygon.append(Vector3(lower_x_bound,upper_y_bound,0))
+            lane.bound_polygon.append(Vector3(upper_x_bound,upper_y_bound,0))
+            lane.bound_polygon.append(Vector3(upper_x_bound,lower_y_bound,0))
+
+            points_filtered_x = []
+            points_filtered_y = []
+            for i in range(points.shape[0]):
+                point_x = points[i, 0]
+                point_y = points[i, 1]
+                if lower_x_bound < point_x and point_x < upper_x_bound and \
+                   lower_y_bound < point_y and point_y < upper_y_bound:
+                    # if True:
+                    # print(point_x, point_y)
+                    points_filtered_x.append(point_x)
+                    points_filtered_y.append(point_y)
+
+            for i in range(len(points_filtered_y)):
+                point_x = points_filtered_x[i]
+                point_y = points_filtered_y[i]
+                lane_pt = Vector3(point_x, point_y, 0.0)    
+                lane.lane_points.append(lane_pt)
+
+            self.lane_pub.publish(lane)
+        except IndexError as e:
+            print(e)
+
+    def skeleton(self, img):
+        # from stackoverflow user Dan Masek (non askii character was replaced with s)
+        # https://stackoverflow.com/questions/42845747/optimized-skeleton-function-for-opencv-with-python
+        skeleton = np.zeros(img.shape,np.uint8)
+        eroded = np.zeros(img.shape,np.uint8)
+        temp = np.zeros(img.shape,np.uint8)
+
+        _,thresh = cv2.threshold(img,127,255,0)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+
+        iters = 0
+        while(True):
+            cv2.erode(thresh, kernel, eroded)
+            cv2.dilate(eroded, kernel, temp)
+            cv2.subtract(thresh, temp, temp)
+            cv2.bitwise_or(skeleton, temp, skeleton)
+            thresh, eroded = eroded, thresh # Swap instead of copy
+
+            iters += 1
+            if cv2.countNonZero(thresh) == 0:
+                return skeleton
+
+    # Define a function for creating lane lines
+    def lane_detector(self, image, video_mode=False):
+        # Image processing
+        hsv_filtered = self.hsv_select(image)
+        skeletoned = self.skeleton(hsv_filtered.astype(np.uint8))
+        # Calculate lane points
+        lane_points_image_frame_yx = np.array(np.where(skeletoned != 0))
+        lane_points_image_frame = np.array([lane_points_image_frame_yx[1,:], lane_points_image_frame_yx[0,:]])
+        # Warp perspective
+        lane_points_map_frame, roi = self.warp_points(lane_points_image_frame, skeletoned.shape)
+        warped_im, roi, Minv = self.warp(np.dstack([skeletoned, skeletoned, skeletoned]))
+        # Publish points and image
+        self.publish_lane_pts(roi, lane_points_map_frame)
+        self.warped_im_pub.publish(bridge.cv2_to_imgmsg(warped_im))
+
+    def image_callback(self, msg):
+        print("Received an image!")
+        try:
+            self.cv2_img = cv2.resize(bridge.imgmsg_to_cv2(msg, "bgr8"), (640, 480))
+        except CvBridgeError as e:
+            print("error recieving image\n", e)
+
+
+    def main(self):
+        rospy.init_node('image_listener')
+        # image_topic = "/zed/image/image_raw"
+        image_topic = "/zed_node/left/image_rect_color"
+        rospy.Subscriber(image_topic, Image, self.image_callback)
+
+        self.raw_pub = rospy.Publisher("raw_image", Image, queue_size=10)
+        self.warped_im_pub = rospy.Publisher("warped_im", Image, queue_size=10)
+        self.debug_img_pub = rospy.Publisher('/debug_img', Image, queue_size=10)
+        self.lane_pub = rospy.Publisher('/lanes', Lane, queue_size=10)
+
+        while not rospy.is_shutdown():
+            if self.cv2_img is not None:
+                self.lane_detector(self.cv2_img)
+                self.cv2_img = None
+
+
+if __name__ == '__main__':
+    detector = LaneDetector()
+    detector.main()
