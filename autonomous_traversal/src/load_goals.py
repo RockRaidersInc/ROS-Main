de82aab56c6bba12e433fe33fd286244f859494e
wus7 <wus7@ubuntu.ubuntu-domain>
2019-05-28 23:32:10 +0000

    Added made the input goals script much easier to use. Wrote a wrapper for setting datum (gps origin frame). Created navigation simulation with use_perfect_odom argument. Fixed the zed camera location for 100th time.

--- autonomous_traversal/src/load_goals.py
+++ autonomous_traversal/src/load_goals.py
@@ -4,17 +4,25 @@ import rospy
 import sys
 import time
 from utils.waypoint_nav import WaypointNavigator
-from utils.general import load_coordinates
+from utils.general import *
 
 
 def main():
 	rospy.init_node('load_goals')
 
-	coordinates_filename = rospy.get_param('coordinates_file')
-	coordinates = load_coordinates(coordinates_filename)
-	orig_lat, orig_lon = coordinates['origin']
-	gps_goals = coordinates['gps_goals']
+	# Get datum from map file and set origins
+	rospy.loginfo('Loading datum from map file')
+	map_name = rospy.get_param('map_name')
+	orig_lat, orig_lon, _ = load_datum_from_map_file(map_name)
+	set_mapviz_origin(orig_lat, orig_lon)
+	set_datum(orig_lat, orig_lon)
 
+	# Load the gps goal coordinates
+	coordinates_filename = rospy.get_param('coordinates')
+	coordinates = load_coordinates(coordinates_filename)
+	gps_goals = coordinates['gps_goals']
+	
+	# Start navigating
 	waypoint_navigator = WaypointNavigator(orig_lat, orig_lon)
 	for goal_num, (goal_lat, goal_lon) in enumerate(gps_goals):
 		waypoint_navigator.send_gps_goal(goal_lat, goal_lon)
--- autonomous_traversal/src/utils/general.py
+++ autonomous_traversal/src/utils/general.py
@@ -4,15 +4,29 @@ import rospy
 import rospkg
 import yaml
 from move_base_msgs.msg import MoveBaseGoal
+from robot_localization.srv import SetDatum
+from geographic_msgs.msg import GeoPose
 
 
 def load_coordinates(filename):
 	rospack = rospkg.RosPack()
 	AT_file_path = rospack.get_path('autonomous_traversal')
 	file_path = '{}/coordinates/{}.yaml'.format(AT_file_path, filename)
-	with open(file_path) as file:
-		coordinates = yaml.safe_load(file)
+	with open(file_path, 'r') as file:
+		coordinates = yaml.load(file)
 	return coordinates
+
+def load_datum_from_map_file(map_name):
+	rospack = rospkg.RosPack()
+	AT_file_path = rospack.get_path('autonomous_traversal')
+	file_path = '{}/maps/{}.yaml'.format(AT_file_path, map_name)
+	with open(file_path, 'r') as file:
+		map_file = yaml.load(file)
+		try:
+			return map_file['datum']
+		except KeyError as ke: 
+			rospy.logwarn('datum not found for map file {}.yaml'.format(map_name))
+			return None
 
 def make_goal(x=0, y=0, thetaZ=1, w=0.):
 	goal = MoveBaseGoal()
@@ -36,3 +50,22 @@ def set_mapviz_origin(orig_lat,orig_lon):
 		rospy.set_param('local_xy_origins', origins)
 	except KeyError:
 		pass
+
+def set_datum(datum_lat, datum_lon):
+	rospy.loginfo('Waiting for service datum')
+	rospy.wait_for_service('datum')
+	try:
+		datum_srv = rospy.ServiceProxy('datum', SetDatum)
+		datum_pose = GeoPose()
+		datum_pose.position.latitude = datum_lat
+		datum_pose.position.longitude = datum_lon
+		datum_pose.position.altitude = 0
+		datum_pose.orientation.x = 0
+		datum_pose.orientation.y = 0
+		datum_pose.orientation.z = 0
+		datum_pose.orientation.w = 1
+		rospy.loginfo('Setting datum to ({}, {})'.format(datum_lat, datum_lon))
+		resp = datum_srv(datum_pose)
+		rospy.loginfo('SetDatum Response: {}'.format(resp))
+	except rospy.ServiceException as se:
+		rospy.roswarn('Service call failed: {}'.format(se))
