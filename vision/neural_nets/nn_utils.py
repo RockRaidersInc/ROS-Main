a131be88cef9c5a88fea6e145dda1b9cb296af0e
David <daweim0@gmail.com>
2019-10-25 14:06:17 -0400

    updated neural net

--- vision/neural_nets/nn_utils.py
+++ vision/neural_nets/nn_utils.py
@@ -3,8 +3,13 @@ import numpy as np
 import cv2
 import torch
 import os
+import functools
+import collections
 
 from confusion_mat_tools import save_confusion_matrix
+
+
+np.random.seed(0)
 
 
 label_name_to_onehot = {"other" :     np.array([1, 0]),
@@ -16,8 +21,37 @@ label_name_to_index = {"other" :     0,
 index_to_label_name = {0: "other",
                        1: "lane"}
 
+
 def to_indices(arr):
     return np.array(list(map(lambda ar: ar.argmax(), [arr[i] for i in range(arr.shape[0])])))
+
+
+# from https://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+class memoized(object):
+    '''Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    '''
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            # uncacheable. a list, for instance.
+            # better to not cache than blow up.
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
 
 
 class data_loader:
@@ -34,15 +68,72 @@ class data_loader:
 
         self.input_image_names = list(map(lambda x: x.strip(), os.listdir(self.input_folder + "/images")))
         random.shuffle(self.input_image_names)
+        self.clear_augmentation()
+
+    def clear_augmentation(self):
+        self.top_left_corner = [0, 0]
+        self.bottom_right_corner = [1, 1]
+        self.reflect_1 = 1
+        self.reflect_2 = 1
+        self.color_scaling = np.array([1, 1, 1])
+        self.color_offset = np.array([0, 0, 0])
+
+        self.images = {}
+        self.labels = {}
+    
+    def update_data_augmentation(self):
+        self.top_left_corner = np.random.randint(0, self.shrunk_width // 4, [2])
+        self.bottom_right_corner = np.random.randint(1, self.shrunk_width // 4, [2])
+        self.reflect_1, self.reflect_2 = np.random.randint(0, 1, [2]) * 2 - 1
+        self.color_scaling = np.random.random([3]) * (1.2 - 0.8) + 0.8  # this produces random floats on the range [0.8, 1.2]
+        self.color_offset = np.random.randint(-20, 20, [3])
+
+        self.images = {}
+        self.labels = {}
+
+    def _augment_image(self, img, change_colors=False):
+        """
+        Data augmentation is a way of "stretching" limited datasets to get more useful training data out of them.
+        The idea is to warp, rotate, and discolor training images so that every time the learning algorithm sees
+        them they are a little different (and effectively somewhat new training data). This isn't as good as
+        having a larger training dataset, but it helps. This function "augments" training images.
+        """
+        
+        # for input images
+        if len(img.shape) == 3:
+            # crop the image a bit
+            padded_shape = np.maximum(img.shape, [self.shrunk_width, self.shrunk_width, 3])
+            padded = np.zeros(np.array(padded_shape) + np.array([1, 1, 0]))
+            padded[:img.shape[0],:img.shape[1], :] =  img
+            img_smaller = padded[self.top_left_corner[0] : -self.bottom_right_corner[0], self.top_left_corner[1] : -self.bottom_right_corner[1], :]
+            # adjust the colors to simulate different lighting conditions
+            color_distorted = np.minimum(255, np.maximum(img_smaller * self.color_scaling + self.color_offset, 0))
+            # reflect the image
+            return color_distorted[::self.reflect_1, ::self.reflect_2]
+        
+        # for label images
+        elif len(img.shape) == 2:
+            # crop the image a bit
+            padded_shape = np.maximum(img.shape, [self.shrunk_width, self.shrunk_width])
+            padded = np.zeros(np.array(padded_shape) + np.array([1, 1]))
+            padded[:img.shape[0],:img.shape[1]] =  img
+            img_smaller = padded[self.top_left_corner[0] : -self.bottom_right_corner[0], self.top_left_corner[1] : -self.bottom_right_corner[1]]
+            # reflect the image. Don't distort colors on the label image
+            return img_smaller[::self.reflect_1, ::self.reflect_2]
+        else:
+            raise Exception()
+        
 
     def __getitem__(self, i):
         if i not in self.images.keys():
-            image = cv2.imread(self.input_folder + "/images/" + self.input_image_names[i], cv2.IMREAD_COLOR)
-            self.images[i] = cv2.resize(image, (self.shrunk_width, self.shrunk_width))[:, :, ::-1]
+            unaugmented = self._read_img_from_disk(self.input_folder + "/images/" + self.input_image_names[i], cv2.IMREAD_COLOR)
+            augmented = self._augment_image(unaugmented, change_colors=True)
+            self.images[i] = cv2.resize(augmented, (self.shrunk_width, self.shrunk_width))[:, :, ::-1]
             
-            label = cv2.imread(self.input_folder + "/gt/" + self.input_image_names[i], cv2.IMREAD_GRAYSCALE)
-            label_resized = cv2.resize(label, (self.label_shrunk_width, self.label_shrunk_width)).astype(np.float32)[:, :] / 128 - 1
-            self.labels[i] = (label_resized > 0.8).astype(np.float32)
+            label = self._read_img_from_disk(self.input_folder + "/gt/" + self.input_image_names[i], cv2.IMREAD_GRAYSCALE)
+            intermediate = self._augment_image(label)
+            label_resized = cv2.resize(intermediate, (self.label_shrunk_width, self.label_shrunk_width), interpolation=cv2.INTER_AREA).astype(np.float32)[:, :]
+            self.labels[i] = (label_resized != 0).astype(np.float32) * 2 - 1
         
         return (self.images[i], self.labels[i])
 
@@ -51,6 +142,16 @@ class data_loader:
 
     def __len__(self):
         return len(self.input_image_names)
+
+    @memoized
+    def _read_img_from_disk(self, path, mode):
+        """
+        Saves a little time by not reading the same image from disk twice. The @memoized annotation means that if
+        that every time this function is called it's inputs and outputs are saved. If it is called with the same 
+        inputs a second timethen the outputs are looked up from the previous run and returned (instead of running 
+        the function again).
+        """
+        return cv2.imread(path, mode)
 
 
 def save_confusion_and_print_errors(confusion, model, test_images, network_name):
--- vision/neural_nets/p2_nn_conv.py
+++ vision/neural_nets/p2_nn_conv.py
@@ -16,15 +16,18 @@ np.set_printoptions(precision=5, suppress=True)
 input_file_list = "image_list.txt"
 shrunk_width = 512
 
-class ConvNet(torch.nn.Module):
-    def __init__(self):
-        super(ConvNet, self).__init__()
-        self.conv1 = torch.nn.Conv2d(3, 8, 3, padding=1)
-        self.conv2 = torch.nn.Conv2d(8, 8, 3, padding=1)
-        self.conv3 = torch.nn.Conv2d(8, 8, 3, padding=1)
-        self.conv4 = torch.nn.Conv2d(8, 1, 3, padding=1)
 
-        self.conv_single = torch.nn.Conv2d(3, 1, 3, padding=1)
+class SmallerNet(torch.nn.Module):
+    """
+    This network appears to generalize quite well, even when only training with the small igvc_sim_trainset_3 dataset
+    (test error is within 3% of training error). It doesn't work super well though.
+    """
+    def __init__(self):
+        super(SmallerNet, self).__init__()
+        self.conv1 = torch.nn.Conv2d(3, 8, 3, padding=1)
+        self.conv2 = torch.nn.Conv2d(8, 12, 3, padding=1)
+        self.conv3 = torch.nn.Conv2d(12, 16, 3, padding=1)
+        self.conv4 = torch.nn.Conv2d(16, 1, 3, padding=1)
 
         self.pool = torch.nn.MaxPool2d(2, 2)
         self.nonlinear = torch.nn.functional.elu
@@ -37,11 +40,47 @@ class ConvNet(torch.nn.Module):
         x1 = self.pool(self.nonlinear(self.conv1(x_prepared)))
         x2 = self.pool(self.nonlinear(self.conv2(x1)))
         x3 = self.pool(self.nonlinear(self.conv3(x2)))
-        x4 = self.pool(self.nonlinear(self.conv4(x2)))
-        output = self.tanh(x4)
+        x4 = self.nonlinear(self.conv4(x3))
+        output = self.tanh(x4)  # using tanh to get a value between -1 and 1
         return output
-        # return self.tanh(self.conv_single(x_prepared))
-        # return x_raw[:, 0, :, :]
+
+
+class LargerNet(torch.nn.Module):
+    def __init__(self):
+        super(LargerNet, self).__init__()
+        self.conv1 = torch.nn.Conv2d(3, 8, 3, padding=1)
+        self.conv2_1 = torch.nn.Conv2d(8, 12, 3, padding=1)
+        self.conv2_2 = torch.nn.Conv2d(12, 12, 1, padding=0)
+        self.conv2_3 = torch.nn.Conv2d(12, 12, 3, padding=1)
+
+        self.conv3_1 = torch.nn.Conv2d(12, 16, 3, padding=1)
+        self.conv3_2 = torch.nn.Conv2d(16, 32, 1, padding=0)
+        self.conv3_3 = torch.nn.Conv2d(32, 16, 3, padding=1)
+
+        self.conv4_1 = torch.nn.Conv2d(16, 36, 3, padding=1)
+        self.conv4_2 = torch.nn.Conv2d(36, 36, 1, padding=0)
+        self.conv4_3 = torch.nn.Conv2d(36, 1, 3, padding=1)
+
+        self.pool = torch.nn.MaxPool2d(2, 2)
+        self.nonlinear = torch.nn.functional.elu
+        self.sigmoid = torch.sigmoid
+        self.tanh = torch.tanh
+        self.dropout = torch.nn.Dropout(0.4)  # drop 50% of the neurons
+    
+    def forward(self, x_raw):
+        x_prepared = x_raw / 128 - 1
+        x1 = self.pool(self.nonlinear(self.conv1(x_prepared)))
+        x2_1 = self.nonlinear(self.conv2_1(x1))
+        x2_2 = self.nonlinear(self.conv2_2(x2_1))
+        x2_3 = self.pool(self.nonlinear(self.conv2_3(x2_2)))
+        x3_1 = self.nonlinear(self.conv3_1(x2_3))
+        x3_2 = self.nonlinear(self.conv3_2(x3_1))
+        x3_3 = self.pool(self.nonlinear(self.conv3_3(x3_2)))
+        x4_1 = self.nonlinear(self.conv4_1(x3_3))
+        x4_2 = self.nonlinear(self.conv4_2(x4_1))
+        x4_3 = self.nonlinear(self.conv4_3(x4_2))
+        output = self.tanh(x4_3)  # using tanh to get a value between -1 and 1
+        return output
 
 
 def main():
@@ -55,11 +94,12 @@ def main():
 
     N = 2  # batch size
 
-    model = ConvNet()
+    model = SmallerNet()
+    # model = LargerNet()
 
     loss_fn = torch.nn.L1Loss()
 
-    learning_rate = 1e-1
+    learning_rate = 1e-2
     # use the ADAM optimizer because it has fewer parameters to tune than SGD
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -71,100 +111,97 @@ def main():
             print("\ron image " + str(i), end='')
 
             # Forward pass: compute predicted y by passing x to the model.
-            next_batch_images = []
+            next_batch_images_unprocessed = []
             next_batch_labels = []
             for j in range(N):
                 image, label = train_images[(i + j) % len(train_images)]
-                # next_batch_images.append(np.swapaxes(image, 0, 2))
+                next_batch_images_unprocessed.append(image)
                 next_batch_labels.append(label)
 
             # batch = torch.from_numpy(np.stack(next_batch_images)).float()
-            batch = prepare_images_for_nn([train_images[(i + j) % len(train_images)][0] for j in range(N)])
+            batch = prepare_images_for_nn(next_batch_images_unprocessed)
             label_images = torch.from_numpy(np.array(next_batch_labels)).float()
 
             predictions = model(batch)
-            # predictions = batch[:, 0, :, :]
+
+            lane_bg_scaling_factor = 1 / (np.mean(np.array(next_batch_labels) / 2 + 0.5) + 0.0001) + 0.75
+            # print("   ", lane_bg_scaling_factor, (np.array(next_batch_labels) / 2 + 0.5).sum(), np.product(np.array(next_batch_labels).shape))
 
             # Compute and print loss.
-            pixelwise_loss = torch.pow(predictions - label_images, 2)
-            loss = torch.mean((label_images + 1) * pixelwise_loss * 100 + pixelwise_loss)
-            print(" \t error: ", loss.data.cpu().numpy())
+            pixelwise_squared_diff = torch.pow(predictions - label_images, 2)
+            lane_pixel_loss = (label_images + 1) / 2 * pixelwise_squared_diff * lane_bg_scaling_factor    # component of loss from pixels on a lane
+            other_pixel_loss = (label_images - 1) / -2 * pixelwise_squared_diff        # component of loss from pixels in the background
+            loss = torch.mean(lane_pixel_loss + other_pixel_loss)
+            # print(" \t loss: ", loss.data.cpu().numpy())
 
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
+            train_images.update_data_augmentation()
+
         print("\r", end="")
         # print("average epoch training accuracy:", epoch_correct_predictions / len(train_images) / np.product(train_images[0][0].shape))
 
-
-        # # now that the network has been trained for one epoch, test it on the training data
-        # model.eval()  # turn off dropout
-        # confusion = np.zeros((2, 2), dtype=np.int32)
-        # for i in range(len(train_images)):
-        #     image, label = train_images[i]
-        #     x = prepare_images_for_nn([image])
-
-        #     prediction = (model(x).data.cpu().numpy() > 0).astype(np.int32)
-        #     label_thresholded = (label > 0).astype(np.int32)
-
-        #     for x in range(prediction.shape[2]):
-        #         for y in range(prediction.shape[3]):
-        #             confusion[label_thresholded[0, x, y], prediction[0, 0, x, y]] += 1
-
-        # print("confusion matrix after epoch", epoch_num)
-        # print("row is correct label, column is predicted label")
-        # for i in range(2):
-        #     print(" " * (11 - len(index_to_label_name[i])) + index_to_label_name[i] + ": ", end="")
-        #     print(confusion[i])
-
-        # n_correct_predictions = np.sum(confusion * np.eye(2, 2)) / np.product(label.shape)
-        # print("training accuracy: %3.4f%%" % (n_correct_predictions / len(train_images) * 100,))
-
-
-
         # now that the network has been trained for one epoch, test it on the testing data
-        model.eval()  # turn off dropout
-        confusion = np.zeros((2, 2), dtype=np.int32)
-        for i in range(len(test_images)):
-            image, label = test_images[i]
-            x = prepare_images_for_nn([image])
+        evaluate_model(model, train_images, epoch_num, train=True, show_images=True if epoch_num % 30 == 29 else False)
+        evaluate_model(model, test_images, epoch_num, train=False, show_images=True if epoch_num % 30 == 29 else False)
 
-            prediction_raw = model(x).data.cpu().numpy().astype(np.float32)
-            prediction = (prediction_raw > 0).astype(np.int32)
-            label_thresholded = (label > 0).astype(np.int32)
+        optimizer.zero_grad()  # just to make sure it doesn't learn from the testing data
 
-            f, (ax1, ax2) = plt.subplots(1, 2)
+    # save the final confusion matrix and print out misclassified images
+    # save_confusion_and_print_errors(confusion, model, test_images, "convolutional_network")
 
-            ax1.imshow(image.squeeze())
+
+def evaluate_model(model, image_set, epoch_num, train=False, show_images=False):
+    model.eval()  # turn off dropout
+    # image_set.clear_augmentation()
+    confusion = np.zeros((2, 2), dtype=np.int32)
+    for i in range(len(image_set)):
+        image, label = image_set[i]
+        x = prepare_images_for_nn([image])
+
+        prediction_raw = model(x).data.cpu().numpy().astype(np.float32)
+        prediction = (prediction_raw > 0).astype(np.int32)
+        label_thresholded = (label > 0).astype(np.int32)
+
+        if show_images:
+            f, (ax1, ax2, ax3) = plt.subplots(1, 3)
+
+            ax1.imshow(image.squeeze().astype(np.uint8))
 
             resized = cv2.resize((np.dstack([prediction_raw.squeeze(), prediction_raw.squeeze(), prediction_raw.squeeze()]) * 127 + 127).astype(np.uint8), 
                                                         dsize=(shrunk_width, shrunk_width), interpolation=cv2.INTER_CUBIC)
 
             # ax2.imshow(image.squeeze())
+            resized[0, 0, 0] = 127
+            resized[0, 1, 0] = -127
             ax2.imshow(resized[:, :, 0] / 127, cmap='jet', alpha=1)
 
-            if epoch_num % 1 == 0:
-                plt.show()
+            label[0, 0] = 1
+            label[0, 1] = -1
+            ax3.imshow(label, cmap='jet', alpha=1)
+            plt.show()
             plt.close(f)
 
-            for x in range(prediction.shape[2]):
-                for y in range(prediction.shape[3]):
-                    confusion[label_thresholded[x, y], prediction[0, 0, x, y]] += 1
+        for x in range(prediction.shape[2]):
+            for y in range(prediction.shape[3]):
+                confusion[label_thresholded[x, y], prediction[0, 0, x, y]] += 1
+    
+    print("confusion matrix after epoch", epoch_num)
+    print("row is correct label, column is predicted label")
+    for i in range(2):
+        print(" " * (11 - len(index_to_label_name[i])) + index_to_label_name[i] + ": ", end="")
+        print(confusion[i])
 
-        print("confusion matrix after epoch", epoch_num)
-        print("row is correct label, column is predicted label")
-        for i in range(2):
-            print(" " * (11 - len(index_to_label_name[i])) + index_to_label_name[i] + ": ", end="")
-            print(confusion[i])
+    n_correct_predictions = np.sum(confusion * np.eye(2, 2)) / np.product(label.shape)
+    if train:
+        print("training accuracy:")
+    else:
+        print("testing accuracy:")
+    print("%3.4f%%" % (n_correct_predictions / len(image_set) * 100,))
 
-        n_correct_predictions = np.sum(confusion * np.eye(2, 2)) / np.product(label.shape)
-        print("testing accuracy: %3.4f%%" % (n_correct_predictions / len(test_images) * 100,))
-
-        optimizer.zero_grad()  # just to make sure it doesn't learn from the testing data
-
-    # save the final confusion matrix and print out misclassified images
-    save_confusion_and_print_errors(confusion, model, test_images, "convolutional_network")
 
 
 def prepare_images_for_nn(imgs):
